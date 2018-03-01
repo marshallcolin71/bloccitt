29f62cba9cba07494c4ffecc4a4385345dc4e338
Colin <marshallcolin71@gmail.com>
2018-02-28 20:19:11 -0500

    re committing

---
+++ bloccit/spec/controllers/welcome_controller_spec.rb
@@ -0,0 +1,33 @@
+require 'rails_helper'
+
+RSpec.describe WelcomeController, type: :controller do
+  describe 'GET index' do 
+    it 'renders the index template' do 
+      get :index
+      expect(response).to render_template("index")
+    end 
+  end
+  
+  describe 'GET about' do 
+    it 'renders the about template' do 
+      get :about
+      expect(response).to render_template("about")
+    end
+  end
+  
+
+  # describe "GET #create" do
+  #   it "returns http success" do
+  #     get :create
+  #     expect(response).to have_http_status(:success)
+  #   end
+  # end
+
+  # describe "GET #spec/conrollers/welcome_controller_spec.rb" do
+  #   it "returns http success" do
+  #     get :spec/conrollers/welcome_controller_spec.rb
+  #     expect(response).to have_http_status(:success)
+  #   end
+  # end
+
+end
---
+++ bloccit/spec/models/topic_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+RSpec.describe Topic, type: :model do
+  let(:topic) { create(:topic) }
+  
+  it { is_expected.to have_many(:posts) }
+  
+  describe "attributes" do 
+    it "has name, description and public attributes" do 
+      expect(topic).to have_attributes(name: topic.name, description: topic.description)
+    end
+    
+    it "is public by default" do 
+      expect(topic.public).to be(true)
+    end
+  end
+end
